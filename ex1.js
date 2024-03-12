ccb4e353476c1933bea13e7adaa07ed54ee3c889
franciscoMello777 <162368791+franciscoMello777@users.noreply.github.com>
2024-03-12 17:24:07 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,6 @@
+let numero1 = 2
+let numero2 = 3
+const soma = numero1 + numero2
+
+console.log(soma)
+//console.log(numero1 + numero2)
---
+++ ex3.js
@@ -0,0 +1,3 @@
+const tempC = 30
+const F = tempC * (9/5) + 32
+console.log(F)
